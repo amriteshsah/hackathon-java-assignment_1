f610ba0a14a788a2ba3464afab95ff79ed541d46
amriteshsah <54805465+amriteshsah@users.noreply.github.com>
2026-03-12 20:49:43 +0530

    Increase product resource coverage for CI jacoco gate

--- src/test/java/com/fulfilment/application/monolith/products/ProductEndpointTest.java
+++ src/test/java/com/fulfilment/application/monolith/products/ProductEndpointTest.java
@@ -2,6 +2,7 @@ package com.fulfilment.application.monolith.products;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.IsNot.not;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -32,5 +33,106 @@ public class ProductEndpointTest {
         .then()
         .statusCode(200)
         .body(not(containsString("TONSTAD")), containsString("KALLAX"), containsString("BESTÅ"));
+  }
+
+  @Test
+  public void shouldCreateUpdateGetAndDeleteProduct() {
+    String uniqueName = "ProductEndpointTest_" + System.currentTimeMillis();
+
+    Number productId =
+        given()
+            .contentType("application/json")
+            .body(
+                "{\"name\":\""
+                    + uniqueName
+                    + "\",\"description\":\"desk\",\"price\":99.99,\"stock\":7}")
+            .when()
+            .post("/product")
+            .then()
+            .statusCode(201)
+            .body("name", equalTo(uniqueName))
+            .body("description", equalTo("desk"))
+            .body("stock", equalTo(7))
+            .extract()
+            .path("id");
+
+    long productIdValue = productId.longValue();
+
+    given()
+        .when()
+        .get("/product/" + productIdValue)
+        .then()
+        .statusCode(200)
+        .body("name", equalTo(uniqueName))
+        .body("description", equalTo("desk"))
+        .body("stock", equalTo(7));
+
+    given()
+        .contentType("application/json")
+        .body(
+            "{\"name\":\""
+                + uniqueName
+                + "_updated\",\"description\":\"chair\",\"price\":149.99,\"stock\":11}")
+        .when()
+        .put("/product/" + productIdValue)
+        .then()
+        .statusCode(200)
+        .body("name", equalTo(uniqueName + "_updated"))
+        .body("description", equalTo("chair"))
+        .body("stock", equalTo(11));
+
+    given().when().delete("/product/" + productIdValue).then().statusCode(204);
+
+    given().when().get("/product/" + productIdValue).then().statusCode(404);
+  }
+
+  @Test
+  public void shouldRejectCreateWhenIdProvided() {
+    given()
+        .contentType("application/json")
+        .body("{\"id\":99,\"name\":\"invalid-product\",\"stock\":1}")
+        .when()
+        .post("/product")
+        .then()
+        .statusCode(422);
+  }
+
+  @Test
+  public void shouldRejectUpdateWhenNameMissing() {
+    String uniqueName = "ProductEndpointTestUpdate_" + System.currentTimeMillis();
+
+    Number productId =
+        given()
+            .contentType("application/json")
+            .body("{\"name\":\"" + uniqueName + "\",\"stock\":2}")
+            .when()
+            .post("/product")
+            .then()
+            .statusCode(201)
+            .extract()
+            .path("id");
+
+    given()
+        .contentType("application/json")
+        .body("{\"description\":\"no-name\",\"stock\":4}")
+        .when()
+        .put("/product/" + productId.longValue())
+        .then()
+        .statusCode(422);
+  }
+
+  @Test
+  public void shouldReturnNotFoundForUnknownProduct() {
+    given().when().get("/product/999999").then().statusCode(404);
+
+    given()
+        .contentType("application/json")
+        .body("{\"name\":\"missing\",\"stock\":1}")
+        .when()
+        .put("/product/999999")
+        .then()
+        .statusCode(404);
+
+    given().when().delete("/product/999999").then().statusCode(404);
   }
 }
